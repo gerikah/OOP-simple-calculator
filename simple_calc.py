b61ffa66da9686cf5ae78a0e1c0d65e41e13b7a0
gerikah <aldaygerikah@gmail.com>
2023-05-23 17:51:59 +0800

    GUI attempt: set colors

--- simple_calc.py
+++ simple_calc.py
@@ -3,11 +3,18 @@
 #import module
 import tkinter as tk
 
+# fonts
 large_font = ("Poppins", 20, "bold")
 small_font = ("Poppins", 15)
 digits_font = ("Poppins", 20, "bold")
 default_font = ("Poppins", 15)
 
+# colors
+off_white = "#09377B"
+white = "#0B69B0"
+light_blue = "#F7D507"
+light_gray = "#FFFFFF"
+label_color = "#000000"
 
 
 
