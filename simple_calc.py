3841b2b2bff37bf223b02933b3d641de5f4fd306
gerikah <aldaygerikah@gmail.com>
2023-05-23 22:25:34 +0800

    GUI attempt: create widgets

--- simple_calc.py
+++ simple_calc.py
@@ -5,9 +5,15 @@ from tkinter import messagebox
 from backend import Calculator
 
 class GUI_Calculator:
-    def __init__(self)
+    def __init__(self):
         self.root = tk.Tk()
         self.root.title("Simple Calculator")
+        self.create_widgets()
+
+    def create_widgets(self):
+        operation_label = tk.Label(self.root, text= "Choose one mathematical operation")
+        operation_label.pack
+
 
 
 
