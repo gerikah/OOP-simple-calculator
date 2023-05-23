f0afdf91c163804e1a32f416efe4d0ebc323aa2b
gerikah <aldaygerikah@gmail.com>
2023-05-23 17:46:00 +0800

    GUI attempt: import tkinter & set fonts

--- simple_calc.py
+++ simple_calc.py
@@ -1,4 +1,15 @@
-# front end file
+# frontend file
+
+#import module
+import tkinter as tk
+
+large_font = ("Poppins", 20, "bold")
+small_font = ("Poppins", 15)
+digits_font = ("Poppins", 20, "bold")
+default_font = ("Poppins", 15)
+
+
+
 
 from back_end import Calculator
 
