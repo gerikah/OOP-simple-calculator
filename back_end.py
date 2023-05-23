f386e11a09c719790af16d84f535edb66019bba6
gerikah <aldaygerikah@gmail.com>
2023-05-23 13:08:28 +0800

    define run_Calculator

--- back_end.py
+++ back_end.py
@@ -7,3 +7,9 @@ class Calculator:
         self.number_2 = None
         self.selected_operation = None
         self.result = None
+
+    def run_Calculator(self):
+        self.get_operation()
+        self.get_numbers()
+        self.perform_calculation()
+        self.display_result()
