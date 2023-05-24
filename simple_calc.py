895753ab62d92ffd4fb28f471a1f22872d68695f
gerikah <aldaygerikah@gmail.com>
2023-05-24 12:52:21 +0800

    GUI attempt: replacing pack with grid

--- simple_calc.py
+++ simple_calc.py
@@ -1,4 +1,3 @@
-# frontend file
 import tkinter as tk
 from tkinter import messagebox
 
@@ -15,36 +14,37 @@ class GUI_Calculator:
 
     def create_widgets(self):
         # label for operation selection
-        operation_label = tk.Label(self.root, text= "Choose one mathematical operation")
-        operation_label.pack
+        operation_label = tk.Label(self.root, text="Choose one mathematical operation")
+        operation_label.grid(row=0, columnspan=4, padx=10, pady=10)
 
-        # button frame
-        button_frame = tk.Frame(self.root)
-        button_frame.pack(side=tk.TOP, padx=10, pady=10)
-        
-        # buttons for each operations
-        add_button = tk.Button(self.root, text = "+", command = lambda: self.set_operation("+"), font = ("Poppins", 15, "bold"), width = 8)
-        add_button.pack(side = tk.LEFT, padx = 10, pady = 10)
-        subtract_button = tk.Button(self.root, text = "-", command = lambda: self.set_operation("-"), font = ("Poppins", 15, "bold"), width = 8)
-        subtract_button.pack(side = tk.LEFT, padx = 10, pady = 10)
-        multiply_button = tk.Button(self.root, text = "x", command = lambda: self.set_operation("*"), font = ("Poppins", 15, "bold"), width = 8)
-        multiply_button.pack(side = tk.LEFT, padx = 10, pady = 10)
-        divide_button = tk.Button(self.root, text = "÷", command = lambda: self.set_operation("/"), font = ("Poppins", 15, "bold"), width = 8)
-        divide_button.pack(side = tk.LEFT, padx = 10, pady = 10)
+        # buttons for each operation
+        add_button = tk.Button(self.root, text="+", command=lambda: self.set_operation("+"),
+                               font=("Poppins", 15, "bold"), width=8)
+        add_button.grid(row=1, column=0, padx=10, pady=10)
+        subtract_button = tk.Button(self.root, text="-", command=lambda: self.set_operation("-"),
+                                    font=("Poppins", 15, "bold"), width=8)
+        subtract_button.grid(row=1, column=1, padx=10, pady=10)
+        multiply_button = tk.Button(self.root, text="x", command=lambda: self.set_operation("*"),
+                                    font=("Poppins", 15, "bold"), width=8)
+        multiply_button.grid(row=1, column=2, padx=10, pady=10)
+        divide_button = tk.Button(self.root, text="÷", command=lambda: self.set_operation("/"),
+                                  font=("Poppins", 15, "bold"), width=8)
+        divide_button.grid(row=1, column=3, padx=10, pady=10)
 
         # entry fields for number 1 and number 2
         self.num1_entry = tk.Entry(self.root)
-        self.num1_entry.pack(side = tk.TOP, padx = 0, pady= 5)
+        self.num1_entry.grid(row=2, column=0, columnspan=4, padx=10, pady=10)
         self.num2_entry = tk.Entry(self.root)
-        self.num2_entry.pack()
+        self.num2_entry.grid(row=3, column=0, columnspan=4, padx=10, pady=10)
 
         # button to calculate
-        calculate_button = tk.Button(self.root, text = "Calculate", command = self.calculate, font=("Poppins", 15, "bold"), width=10)
-        calculate_button.place(relx=0.5, rely=1.0, anchor="s")
+        calculate_button = tk.Button(self.root, text="Calculate", command=self.calculate,
+                                     font=("Poppins", 15, "bold"), width=10)
+        calculate_button.grid(row=4, columnspan=4, padx=10, pady=10)
 
         # displaying result label
         self.result_label = tk.Label(self.root)
-        self.result_label.pack()
+        self.result_label.grid(row=5, columnspan=4, padx=10, pady=10)
 
     def run_calculator(self):
         self.root.mainloop()
@@ -63,11 +63,8 @@ class GUI_Calculator:
             messagebox.showerror("Error, You cannot divide by zero", str(error))
 
     def display_result(self, result):
-        self.result_label.config(text = "The result is {}".format(result))
+        self.result_label.config(text="The result is {}".format(result))
 
 if __name__ == "__main__":
     calculator_gui = GUI_Calculator(Calculator)
     calculator_gui.run_calculator()
-
-
-    
