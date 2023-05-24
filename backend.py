a41c6871d0752a838c8d082d4cbc87cba1de5382
gerikah <aldaygerikah@gmail.com>
2023-05-24 09:41:30 +0800

    GUI attempt: removing more unnecessary lines at the backend file

--- backend.py
+++ backend.py
@@ -7,40 +7,7 @@ class Calculator:
         self.number_2 = None
         self.selected_operation = None
         self.result = None
-
-    def run_Calculator(self):
-        while True:
-            self.get_operation()
-            self.get_numbers()
-            self.perform_calculation()
-            self.display_result()
-            if not self.try_again():
-                print("Thank you for using this calculator")
-                break
-
-    def get_operation(self):
-        while True:
-            try:
-                self.selected_operation = input()
-                if self.selected_operation not in self.mathematical_operations:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Inavalid operation. Please try again.")
-
-    def get_numbers(self):
-        while True:
-            try:
-                print("Enter the first number: ")
-                self.number_1 = float(input())
-                print("Enter the second number: ")
-                self.number_2 = float(input())
-                if not isinstance(self.number_1, float) or not isinstance(self.number_2, float):
-                    raise ValueError
-                break
-            except ValueError:
-                print("Invalid input. Please try again")
-
+    
     def perform_calculation(self):
         try:
             self.selected_operation = self.operation_Variable.get()
