83674ebb3323a8c66ebebc0f7cdec07b626521f2
gerikah <aldaygerikah@gmail.com>
2023-06-04 22:29:46 +0800

    comments added

--- subclass.py
+++ subclass.py
@@ -1,9 +1,12 @@
+# import Calculatro class from backend file
 from backend import Calculator
- 
+
+# new class for new operations
 class Advanced_Calculator(Calculator):
     def __init__(self):
         super().__init__()
 
+    # exponent operation
     def exponent (self, base, exponent):
         self.number_1 = base
         self.number_2 = exponent
@@ -11,6 +14,7 @@ class Advanced_Calculator(Calculator):
         self.perform_calculation()
         return self.result
     
+    # remainder operation
     def remainder(self, dividend, divisor):
         self.number_1 = dividend
         self.number_2 = divisor
