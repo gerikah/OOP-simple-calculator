4f7e11b0e6e5ec90f0d8317329847d32d4c6507d
gerikah <aldaygerikah@gmail.com>
2023-06-04 21:44:02 +0800

    setting operation exponent

--- subclass.py
+++ subclass.py
@@ -4,5 +4,9 @@ class Advanced_Calculator(Calculator):
     def __init__(self):
         super().__init__()
 
-    def exponent (self, base, number):
-        
+    def exponent (self, base, exponent):
+        self.number_1 = base
+        self.number_2 = exponent
+        self.selected_operation = "**"
+        self.perform_calculation()
+        return self.result
